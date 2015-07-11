d3aea93ac0e1ff11dafb09d97942cf0a615b55e9
Quy Tran <quytran288@gmail.com>
2015-07-11 12:00:12 +0700

    Finish app without css. What i learn from this app is: - Seperate controller, services - Create custom filter search - Use route to configure url - use $http to get JSON from remote server with method: JSONP

--- Practice/F1Feeder/app/js/app.js
+++ Practice/F1Feeder/app/js/app.js
@@ -1,1 +1,7 @@
 var F1FeederApp = angular.module('F1FeederApp',['ngRoute','F1FeederApp.controllers','F1FeederApp.services']);
+F1FeederApp.config(function($routeProvider){
+	$routeProvider
+		.when("/drivers",{ templateUrl : 'partials/drivers.html', controller : 'driversController'})
+		.when("/drivers/:id",{ templateUrl : 'partials/driver.html', controller : 'driverController'})
+		.otherwise({redirectTo : "/drivers"});
+})
--- Practice/F1Feeder/app/js/controllers.js
+++ Practice/F1Feeder/app/js/controllers.js
@@ -5,4 +5,22 @@ angular.module("F1FeederApp.controllers",[])
 		ergastAPIservice.getDrivers().success(function(response){
 			$scope.driversList = response.MRData.StandingsTable.StandingsLists[0].DriverStandings;
 		})
+		$scope.searchFilter = function(driver){
+			var keyword = new RegExp($scope.nameFilter,'i');
+			return !$scope.nameFilter || keyword.test(driver.Driver.givenName) || keyword.test(driver.Driver.familyName);
+		}
+	})
+	.controller('driverController',function($scope,ergastAPIservice,$routeParams){
+		$scope.id = $routeParams.id;
+		$scope.driver = null;
+		$scope.races = [];
+
+		ergastAPIservice.getDriverDetails($scope.id).success(function (response) {
+	        $scope.driver = response.MRData.StandingsTable.StandingsLists[0].DriverStandings[0]; 
+	    });
+
+		ergastAPIservice.getDriverRaces($scope.id).success(function (response) {
+			$scope.races = response.MRData.RaceTable.Races;
+		});
+
 	})
