b9bc6b940afd7ef0aadee59a86d21a2a9043bbf9
Quy Tran <quy.tran@bluecomgroup.com>
2015-08-05 16:08:07 +0700

    - Finished Grid app

--- Practice/dataGrid/app/controller.js
+++ Practice/dataGrid/app/controller.js
@@ -1,12 +1,37 @@
 'use strict';
 
 angular.module('gridCtrl',[])
-	.controller('gridMainCtrl',function($scope,gridAPI){
+	.controller('gridMainCtrl',function($scope,gridAPI,$timeout){
 		function getCustomers(){
 			gridAPI.getCustomers().success(function(response){
-				$scope.customerLists = response;
+				$scope.list = response;
+				$scope.currentPage = 1;
+				$scope.entryLimit = 5;
+				$scope.filteredItems = $scope.list.length;
+				$scope.totalItems = $scope.list.length;
 			})
 		}
 
 		getCustomers();
+
+		$scope.sort_by = function(predicate){
+			$scope.predicate = predicate;
+			$scope.reverse = ! $scope.reverse
+		}
+
+		$scope.filter = function(){
+			$timeout(function(){
+				$scope.filteredItems = $scope.filtered.length;
+			},10);
+		}
+
+	}).filter('startFrom',function(){
+		return function(input,start){
+			if (input) {
+				start = +start;
+				return input.slice(start);
+			}
+
+			return [];
+		}
 	})
